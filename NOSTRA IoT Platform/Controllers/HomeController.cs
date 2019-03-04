cc7fb1c76fb39b7885bea679cb0615ebe9fbbcb3
nitipong <goddy_2556@hotmail.com>
2019-03-04 15:41:59 +0700

    First commit

---
+++ NOSTRA IoT Platform/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NOSTRA_IoT_Platform.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.markdashboard = "active";
+            return View();
+        }
+
+        public ActionResult Timeline()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.marktimeline = "active";
+            return View();
+        }
+
+        public ActionResult Sensor()
+        {
+            ViewBag.Message = "Your contact page.";
+            ViewBag.marksensor = "active";
+            return View();
+        }
+
+        public ActionResult Analysis()
+        {
+            ViewBag.Message = "Your contact page.";
+            ViewBag.markanalysis = "active";
+            return View();
+        }
+    }
+}
---
+++ NOSTRA IoT Platform/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NOSTRA_IoT_Platform.MvcApplication" Language="C#" %>
